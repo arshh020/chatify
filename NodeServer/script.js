a8b19128b179d8f0e90a620874fcb21c191feaa3
Arsh Gogia <arshgogia20@gmail.com>
2024-03-04 21:50:19 +0400

    feat: backend completed

--- NodeServer/script.js
+++ NodeServer/script.js
@@ -8,19 +8,27 @@ const io = require("socket.io")(server, {
 server.listen(8000);
 const users = {};
 
+//socket.io instance, will listen to connections
 io.on("connection", (socket) => {
-  //socket.io instance, will listen to connections
+  //particular connection
+  //if any new user joins, let other users connected to the server know
   socket.on("new-user-joined", (username) => {
-    //particular connection
-    console.log("New user", username);
     users[socket.id] = username;
     socket.broadcast.emit("user-joined", username);
   });
 
+  //if someone sends a message, broadcast to other people
   socket.on("send", (message) => {
     socket.broadcast.emit("receive", {
       message: message,
       username: users[socket.id],
     });
+  });
+
+  //if someone leaves the chat, let others know
+  socket.on("disconnect", (message) => {
+    //built-in event
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
   });
 });
--- client.js
+++ client.js
@@ -1,7 +1,49 @@
 const socket = io("http://localhost:8000");
 
+//get DOM elements in respective JS variables
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
 const messageContainer = document.querySelector(".message-box");
+
+//audio will play on receiving messages
+var audio = new Audio("chatify_notif.mp3");
+
+//function which will append event info to the conatainer
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+  if (position == "left") {
+    audio.play();
+  }
+};
+
+//ask new user for his/her name and let the server know
 const username = prompt("Enter your name to join");
 socket.emit("new-user-joined", username);
+
+//if new user joins, receive his/her name from the server
+socket.on("user-joined", (username) => {
+  append(`${username} joined the chat`, "left");
+});
+
+//if server sends a message, receive it
+socket.on("receive", (data) => {
+  append(`${data.username}: ${data.message}`, "left");
+});
+
+//if a user leaves the chat, append the info to the container
+socket.on("left", (data) => {
+  append(`${username} left the chat`, "left");
+});
+
+//if the form gets submitted, send server the messages
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`you: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
